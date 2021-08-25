1e535c321c03f8a3ce1011b3e24dbf1968e90c27
Gio Estrada <giovanni.estrada@unosquare.com>
2021-08-25 16:19:56 -0500

    Catching duplicated email at user creation

--- chat-api/src/services/repositories/impl/user.repository.ts
+++ chat-api/src/services/repositories/impl/user.repository.ts
@@ -1,6 +1,7 @@
 import { User } from '../../../../models/user';
 import { UserRepository } from '../interfaces/user.repository';
 import { userDTO } from '../../../../DTOs/userDTO';
+import { ApplicationException } from '../../../common/exceptions/application.exception';
 
 export class UserSequelizeRepository implements UserRepository {
 	public async findByID(userID: number): Promise<User | null> {
@@ -12,6 +13,18 @@ export class UserSequelizeRepository implements UserRepository {
 	}
 
 	public async add(newUser: userDTO): Promise<User | null> {
-		return await User.create(newUser);
+		return await User.create(newUser)
+			.then((result) => {
+				return result;
+			})
+			.catch((err) => {
+				const { errors } = err;
+				const duplicatedEmail = errors.some(
+					(err: { message: string }) =>
+						err.message === 'users.email must be unique'
+				);
+				console.log(duplicatedEmail);
+				throw new ApplicationException('The provided email is already in use.');
+			});
 	}
 }
